5fc781490d640d04aeb749440eee4220021210b6
Yaroslava Rusakova <yarus7777@gmail.com>
2016-02-24 13:38:34 +0300

    no message

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -1,13 +1,18 @@
 ﻿using Assets.Scripts.Effects;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts {
-    public class LoadLevel : MonoBehaviour {
+    public class LoadLevel : MonoBehaviour
+    {
+        public Button playBtn;
 
         // Use this for initialization
-        public void LoadingLevel (string level) {
+        public void LoadingLevel (string level)
+        {           
             SoundManager.Instance.Play(FxType.MenuButton);
             Application.LoadLevelAsync (level);
+            playBtn.interactable = false;
         }
     }
 }
